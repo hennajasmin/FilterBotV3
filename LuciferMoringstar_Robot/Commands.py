a8409d70be48814ed8917e9aefb3ede73425d948
hennajasmin <79416155+hennajasmin@users.noreply.github.com>
2022-01-14 23:22:18 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -84,7 +84,7 @@ async def start(bot, message):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🎖 DEPLOY YOURS 🎖', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('🎖 DEPLOY YOURS 🎖',  url=invite_link.invite_link
                     ]
                     ]
                 await bot.send_cached_media(
